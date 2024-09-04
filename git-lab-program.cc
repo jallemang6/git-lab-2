2e79e87af8c83fe14422d0070880a1e2b79f72d8
Jenna Allemang <ja334822@ohio.edu>
2024-09-04 15:45:22 -0400

    Final commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Jenna Allemang>
+ *        Date: <Sep 4th 2024>
+ * Description: Git Lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
